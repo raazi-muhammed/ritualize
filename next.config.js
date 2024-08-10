fdf7856460226298119aca7b03380a9a0dc8e9ed
Raazi Muhammed K <raazi@fleapo.com>
2024-08-10 16:44:12 +0530

    fix: pwa not working

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const withPWA = require("next-pwa")({
-    dest: "public",
-});
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+const nextConfig = {
+    ...withPWA({
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+    }),
+};
 
-module.exports = withPWA({
-    // next.js config
-});
+module.exports = nextConfig;
