f28ae9e5c102a19dfa1b2c6d44f522f32ba4369c
Raazi Powder <raazi6163@gmail.com>
2024-11-16 11:41:46 +0530

    feat: add animations

--- src/app/(routine)/[id]/start/_components/StartComponent.tsx
+++ src/app/(routine)/[id]/start/_components/StartComponent.tsx
@@ -5,7 +5,9 @@ import { useStopwatch } from "@/hooks/stop-watch";
 import { Routine, Task } from "@prisma/client";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "motion/react"
+
 
 function StartComponent({
     routine,
@@ -14,10 +16,21 @@ function StartComponent({
         tasks: Task[];
     };
 }) {
+    const myRef = useRef(null);
+
     const router = useRouter();
     const tasks = routine?.tasks;
     const [currentTaskIndex, setCurrentTaskIndex] = useState<number>(0);
     const { time, reset } = useStopwatch();
+    const [moveBy, setMoveBy] = useState(0 + 200)
+
+    useEffect(()=>{
+        const item = document.getElementById("active-task")
+        item?.scrollIntoView({
+            block: "start",
+            behavior: "smooth",
+        })
+    }, [currentTaskIndex])
     return (
         <main className="container relative flex h-[100svh] w-full flex-col justify-around">
             <header>
@@ -43,11 +56,53 @@ function StartComponent({
                     ))}
                 </section>
             </header>
-            <footer>
-                <section className="my-8">
-                    <Heading>{tasks[currentTaskIndex].name}</Heading>
-                    <small>{tasks[currentTaskIndex].duration} minutes</small>
+            <main className="h-[calc(100vh-20rem)] overflow-scroll" ref={myRef}>
+            <section className="flex flex-col gap-2 h-[calc(100vh-30rem)] py-30 mt-24 mb-32" >
+                {routine.tasks.map((task, index) => (
+                            <motion.div
+                            key={task.id}
+                            className="scroll-mt-[20vh]"
+                            id={currentTaskIndex==index ? "active-task" : "in-active-task"}
+                             initial={{
+                                scale:.75,
+                                originX: 0,
+                                opacity: .10
+                            }}
+                            animate={{
+                                scale: currentTaskIndex==index ? 1 : .75,
+                                originX: 0,
+                                opacity: currentTaskIndex==index ? 1 : .25,
+                            }}
+                            transition={{
+                                duration: .45, 
+                                
+                            }}
+                            onClick={()=>{
+                                setCurrentTaskIndex(index)
+                            }}
+                            >
+
+                            <Heading >  {task.name}
+                            </Heading>
+                            <motion.small
+                            initial={{
+                                opacity: 0
+                            }} animate={{
+                                opacity: currentTaskIndex==index ? 1 : 0,
+                            }}
+                            transition={{
+                                duration: .75
+                            }}
+                            
+                            >{tasks[currentTaskIndex].duration} minutes</motion.small>
+
+                            </motion.div>
+                        
+                    ))}
                 </section>
+            </main>
+            <footer>
+                
                 <section className="flex justify-between">
                     <div className="grid gap-1">
                         <Button
@@ -80,6 +135,7 @@ function StartComponent({
                                 onClick={() => {
                                     setCurrentTaskIndex((cti) => ++cti);
                                     reset();
+                                    
                                 }}>
                                 Next
                                 <ChevronRight className="-me-2" />
