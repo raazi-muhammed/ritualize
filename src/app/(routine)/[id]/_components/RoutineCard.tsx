216e4bcacddb26d1ae5c1e72df0d4410d8a13c45
Raazi Muhammed K <raazi6163@gmail.com>
2025-12-19 22:59:12 +0530

    ui: change colors

--- src/app/(routine)/[id]/_components/RoutineCard.tsx
+++ src/app/(routine)/[id]/_components/RoutineCard.tsx
@@ -4,49 +4,52 @@ import { Routine } from "@prisma/client";
 import { Heart } from "lucide-react";
 
 const RoutineCard = ({
-    routine,
-    isList = false,
+  routine,
+  isList = false,
 }: {
-    routine: Routine;
-    isList?: boolean;
+  routine: Routine;
+  isList?: boolean;
 }) => {
-    return (
-        <Card
-            className={`relative -z-0 overflow-hidden p-2 ${
-                routine?.id ?? "opacity-50 pointer-events-none"
-            }`}>
-            <CardHeader className="z-10 p-2 flex justify-between flex-row">
-                <div
-                    className={`flex ${
-                        isList ? "flex-col" : "flex-row"
-                    } gap-2 align-middle`}>
-                    <div
-                        className={`bg-secondary shadow-inner shadow-secondary-border ${
-                            isList ? "size-12" : "size-8"
-                        } grid place-items-center rounded-full`}>
-                        <Icon
-                            name={routine.icon as any}
-                            size={isList ? "1.5rem" : "1rem"}
-                        />
-                    </div>
-                    <div>
-                        <CardTitle className="flex justify-between text-lg my-auto font-medium">
-                            {routine.name}
-                        </CardTitle>
-                        <small className="text-muted-foreground">
-                            {routine.type}
-                        </small>
-                    </div>
-                </div>
+  return (
+    <Card
+      className={`relative -z-0 overflow-hidden p-2 ${
+        routine?.id ?? "opacity-50 pointer-events-none"
+      }`}
+    >
+      <CardHeader className="z-10 p-2 flex justify-between flex-row">
+        <div
+          className={`flex ${
+            isList ? "flex-col" : "flex-row"
+          } gap-2 align-middle`}
+        >
+          <div
+            className={`bg-secondary shadow-inner shadow-secondary-border ${
+              isList ? "size-10" : "size-8"
+            } grid place-items-center rounded-full`}
+          >
+            <Icon
+              name={routine.icon as any}
+              size={isList ? "1.5rem" : "1rem"}
+            />
+          </div>
+          <div className="flex flex-col gap-0">
+            <CardTitle className="flex justify-between text-base my-auto font-medium line-clamp-1">
+              {routine.name}
+            </CardTitle>
+            <small className="text-xs text-muted-foreground">
+              {routine.type}
+            </small>
+          </div>
+        </div>
 
-                {routine.is_favorite ? (
-                    <Heart className="fill-primary text-primary" size={18} />
-                ) : (
-                    <Heart className="text-secondary" size={18} />
-                )}
-            </CardHeader>
-        </Card>
-    );
+        {routine.is_favorite ? (
+          <Heart className="fill-primary text-primary" size={18} />
+        ) : (
+          <Heart className="text-secondary" size={18} />
+        )}
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default RoutineCard;
