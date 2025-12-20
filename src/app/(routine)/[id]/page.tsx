cb813d6daa8b54c3b9d06042a23f96074d82133d
Raazi Muhammed K <raazi6163@gmail.com>
2025-12-20 12:19:03 +0530

    move to actions

--- src/app/(routine)/[id]/page.tsx
+++ src/app/(routine)/[id]/page.tsx
@@ -3,17 +3,24 @@
 export const dynamic = "force-static";
 
 import RoutinePage from "./_components/RoutinePage";
-import InfoMessage from "@/components/message/InfoMessage";
 import { useStore } from "@/stores";
 import DateSelector from "@/app/_components/DateSelector";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { RoutineWithTasks } from "@/types/entities";
 import RoutineSkeleton from "../_components/RoutineSkeleton";
+import PageTemplate from "@/components/layout/PageTemplate";
+import ContentStateTemplate from "@/components/layout/ContentStateTemplate";
+import { IoAddCircle } from "react-icons/io5";
+import { useModal } from "@/providers/ModelProvider";
+import RoutineForm, { routineSchema } from "../_forms/RoutineForm";
+import { z } from "zod";
+import { createRoutine } from "@/services/routines";
 
 export default function Page({ params }: { params: { id: string } }) {
   const routineId = params.id;
   const { selectedDate } = useStore((state) => state);
-
+  const { openModal, closeModal } = useModal();
+  const queryClient = useQueryClient();
   const { data: routine, isFetching } = useQuery({
     queryKey: ["routine", routineId, selectedDate],
     queryFn: async () => {
@@ -27,18 +34,43 @@ export default function Page({ params }: { params: { id: string } }) {
     },
   });
 
-  if (isFetching) return <RoutineSkeleton />;
+  const { mutateAsync } = useMutation({
+    mutationFn: createRoutine,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["routines"],
+      });
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof routineSchema>) {
+    closeModal();
+    mutateAsync(values);
+  }
 
   return (
-    <>
-      {!routine?.id ? (
-        <InfoMessage message="Routine not found" />
-      ) : (
-        <div>
-          <RoutinePage routine={routine} />
-          <DateSelector />
-        </div>
-      )}
-    </>
+    <PageTemplate
+      title={routine?.name || "Routine"}
+      actions={[
+        {
+          label: "Add",
+          icon: IoAddCircle,
+          onClick: () => {
+            openModal({
+              title: "Add Routine",
+              content: <RoutineForm onSubmit={onSubmit} />,
+            });
+          },
+        },
+      ]}
+    >
+      <ContentStateTemplate
+        isLoading={isFetching}
+        skeleton={<RoutineSkeleton />}
+      >
+        {routine && <RoutinePage routine={routine} />}
+        <DateSelector />
+      </ContentStateTemplate>
+    </PageTemplate>
   );
 }
