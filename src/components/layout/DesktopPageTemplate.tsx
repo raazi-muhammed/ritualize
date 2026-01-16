302ced574847cee14515d98f23ab14b8c7f95aa5
Raazi Muhammed K <raazi6163@gmail.com>
2026-01-16 22:42:10 +0530

    refactor: move to shadcn sidebar

--- src/components/layout/DesktopPageTemplate.tsx
+++ src/components/layout/DesktopPageTemplate.tsx
@@ -1,14 +1,22 @@
 import RoutineList from "@/app/(routine)/_components/RoutineList";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 import React from "react";
 
 const DesktopPageTemplate = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex">
-      <div className="w-[20vw] border-2 border-red-500">
-        <RoutineList />
-      </div>
-      <div className="w-[80vw] border-2 border-green-500">{children}</div>
-    </div>
+    <SidebarProvider>
+      <Sidebar>
+        <SidebarContent className="p-4">
+          <RoutineList />
+        </SidebarContent>
+      </Sidebar>
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
   );
 };
 
