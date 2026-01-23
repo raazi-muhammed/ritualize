27d5a82436a2e24ee33f0b7bc7f8152028bbb957
Raazi Muhammed K <raazi6163@gmail.com>
2026-01-24 01:49:45 +0530

    fix: may be

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,5 +3,11 @@ import Google from "@auth/core/providers/google";
 import { Password } from "@convex-dev/auth/providers/Password";
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [Password, Google],
+  providers: [
+    Password,
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
 });
