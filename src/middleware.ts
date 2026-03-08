320c0347551b67f366388e081384cf53b10972d2
Raazi Muhammed K <raazi6163@gmail.com>
2026-03-08 11:45:34 +0530

    fix: logout issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
 
-export default convexAuthNextjsMiddleware();
+export default convexAuthNextjsMiddleware(undefined, {
+  cookieConfig: {
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+  },
+});
 
 export const config = {
   // The following matcher runs middleware on all routes
